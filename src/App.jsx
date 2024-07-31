c4c9ff062e689ca761aa01a3236c3ba53aee2a10
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-07-31 12:27:31 -0700

    task8

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CardGrid from "./components/card";
+import Hero from "./components/heroSection";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <CardGrid />
+    </>
+  );
+}
+
+export default App;
